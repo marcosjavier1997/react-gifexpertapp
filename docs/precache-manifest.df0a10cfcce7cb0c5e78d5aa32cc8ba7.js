773feb39d1b14256b9603657714203a64c6e7705
Marcos Gomez <marcos_go2015@hotmail.com>
2020-10-19 17:30:49 -0300

    docs added

---
+++ docs/precache-manifest.df0a10cfcce7cb0c5e78d5aa32cc8ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1ace767edf3db2c76eaa7bd2da6e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16599bc364a2ec6a01f2",
+    "url": "/static/css/main.62ba7c13.chunk.css"
+  },
+  {
+    "revision": "c148df94a9d083842dcc",
+    "url": "/static/js/2.fa9c1b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa9c1b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16599bc364a2ec6a01f2",
+    "url": "/static/js/main.9e704c1d.chunk.js"
+  },
+  {
+    "revision": "055f3e008571789170d2",
+    "url": "/static/js/runtime-main.3e4d2dea.js"
+  }
+]);
